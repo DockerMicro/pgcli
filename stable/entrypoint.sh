5173081d80a907b55004b87bf3daafd376f859fd
Maciej Lisiewski <maciej.lisiewski@gmail.com>
2016-10-06 18:45:41 -0400

    Use the same default user as official postgres docker image

--- stable/entrypoint.sh
+++ stable/entrypoint.sh
@@ -4,6 +4,9 @@ connection_url=$1
 if [ -n "$connection_url" ]; then
     /usr/bin/pgcli "$connection_url"
 elif [ -n "$POSTGRES_PORT_5432_TCP_ADDR" ]; then
+    if [ -z "$POSTGRES_ENV_POSTGRES_USER" ]; then
+        POSTGRES_ENV_POSTGRES_USER='postgres'
+    fi
     /usr/bin/pgcli postgres://$POSTGRES_ENV_POSTGRES_USER:$POSTGRES_ENV_POSTGRES_PASSWORD@$POSTGRES_PORT_5432_TCP_ADDR:$POSTGRES_PORT_5432_TCP_PORT
 else
     echo "Database connection url missing"
